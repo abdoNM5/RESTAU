8cf65ad25f794c507950dbfe678df9438c782538
ABDELKADER <nmiraabdellader@gmail.com>
2024-11-04 22:27:53 +0100

    last updates of the menu

--- menu/headerrestau.h
+++ menu/headerrestau.h
@@ -1,1 +1,69 @@
 
+// Functions to display sub-menu for each category
+void displayBoissons() {
+    printf("\n--- Boissons ---\n");
+    printf("1. Café - Ingrédients: Eau, Café moulu\n");
+    printf("2. Thé - Ingrédients: Eau, Thé vert\n");
+    printf("3. Eau minérale - \n");
+    printf("4. Retour au menu principal\n");
+     printf("5. Jus d'orange - \n");
+      printf("6. Jus de carotte - \n");
+       printf("7. Jus de citron - \n");
+        printf("8. Jus de bannane - \n");
+         printf("9. Jus panache- \n");
+}
+
+void displayPetitDejeuner() {
+    printf("\n--- Petit Déjeuner ---\n");
+    printf("1.\n--- Ftour Beldi ---\n");
+    printf("Ingrédients: Msemen, Beghrir, Zitoune (olives), Fromage, Thé à la menthe\n");
+    printf("2.\n--- Ftour Chamali ---\n");
+    printf("Ingrédients: Krachel, Batbout, Amlou, Oeufs au cumin, Zaatar, Thé au thym\n");
+   printf("3.\n--- Ftour Bidawi ---\n");
+    printf("Ingrédients: Harcha, Meloui, Beurre, Miel, Jben (fromage frais), Thé à la menthe\n");
+     printf("4.\n--- Ftour Fessi ---\n");
+    printf("Ingrédients: Khlii (viande séchée), Oeufs, Msemen, Zitoune, Pain de semoule, Thé à la menthe\n");
+    printf("5.\n--- Ftour Espagnol ---\n");
+    printf("Ingrédients: Tortilla espagnole, Churros, Pain à la tomate, Café au lait\n");
+    printf("6.\n--- Ftour Hollandais ---\n");
+    printf("Ingrédients: Pannenkoeken (crêpes hollandaises), Fromage gouda, Jus d'orange, Café\n");
+    printf("7.\n--- Ftour Merrakchi ---\n");
+    printf("Ingrédients: Bissara (purée de fèves), Khobz marqa (pain traditionnel), Rghaif, Zitoune, Thé à la menthe\n");
+     printf("8.\n--- Ftour Norvegien ---\n");
+    printf("Ingrédients: Saumon fumé, Oeufs brouillés, Pain complet, Fromage à la crème, Jus d'orange\n");
+    printf("9.\n--- Ftour Sportif ---\n");
+    printf("Ingrédients: Flocons d'avoine, Banane, Yaourt grec, Amandes, Jus de fruits\n");
+    printf("10.\n--- Ftour Express ---\n");
+    printf("Ingrédients: Croissant, Jus d'orange, Café\n");
+}
+
+void displayDejeuner() {
+      printf("\n--- Menu Déjeuner ---\n");
+        printf("1.\n--- Couscous Royal ---\n");
+        printf("Ingrédients: Semoule, Viande d'agneau, Légumes (carottes, courgettes, pois chiches), Ras el hanout\n");
+        printf("2.\n--- Tajine de Poulet aux Citrons Confits et Olives ---\n");
+        printf("Ingrédients: Poulet, Citrons confits, Olives vertes, Safran, Gingembre\n");
+        printf("3.\n--- Pastilla au Poulet ---\n");
+        printf("Ingrédients: Feuilles de brick, Poulet, Amandes, Cannelle, Sucre glace\n");
+        printf("4.\n--- Brochettes de Viande ---\n");
+        printf("Ingrédients: Boeuf ou agneau, Épices marocaines, Ail, Persil\n");
+        printf("5.\n--- Salade Marocaine ---\n");
+        printf("Ingrédients: Tomates, Concombres, Poivrons, Oignons, Coriandre\n");
+        printf("6. Retour au menu principal\n");
+}
+
+void displayDiner() {
+      printf("\n--- Menu Dîner ---\n");
+        printf("1.\n--- Harira ---\n");
+        printf("Ingrédients: Tomates, Lentilles, Pois chiches, Viande d'agneau, Céleri, Coriandre\n");
+        printf("2.\n--- Tajine de Kefta aux Oeufs ---\n");
+        printf("Ingrédients: Viande hachée, Sauce tomate, Oeufs, Épices marocaines, Persil\n");
+        printf("3.\n--- Méchoui ---\n");
+        printf("Ingrédients: Agneau rôti, Beurre, Cumin, Sel\n");
+        printf("4.\n--- Poisson Chermoula ---\n");
+        printf("Ingrédients: Poisson (daurade ou sardine), Coriandre, Persil, Ail, Paprika, Cumin\n");
+        printf("5.\n--- Briouates ---\n");
+        printf("Ingrédients: Feuilles de brick, Farce (poulet ou légumes), Fromage, Cannelle\n");
+        printf("6. Retour au menu principal\n");
+}
+
